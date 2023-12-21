f9dfb3a0cd5e8bc4901761a262ebbc2eb750a4f1
hatakekakashi2001 <152386567+hatakekakashi2001@users.noreply.github.com>
2023-12-21 07:05:58 +0000

    first chapter of js

---
+++ js/lesson1/01_variables.js
@@ -0,0 +1,30 @@
+// to declare variables 
+// one is to declare constant which does not change 
+// other is to decalre a variable 
+
+// so two keywords are const and let
+
+const accountId=98943
+let accountName='Rahul'
+
+console.log(accountName)
+console.table([accountId,accountName])
+
+// datatypes available is js are 
+// 1.Number
+// 2.string
+// 3.boolean
+// 4.null
+// 5.undefined
+
+// when you does not initailize the value then it is stored as undefined
+// typeof function is used to find the data type
+
+let phone=927627
+let model='realme'
+let processor
+let state=true
+let casegiven=null
+
+console.table([typeof phone,typeof model,typeof processor,typeof(state),typeof(casegiven)])
+
